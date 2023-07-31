9fcaa4622f3f8fac94e1d0e2e15306cfdbf32bee
Your Name <you@example.com>
2023-07-31 14:53:28 +0700

    sửa App router

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
-import App from './App';
+import { DevSupport } from "@react-buddy/ide-toolbox";
+import { ComponentPreviews, useInitial } from "./dev";
 import reportWebVitals from './reportWebVitals';
+import App from './App';
 import './index.css';
 
 const container = document.getElementById('root');
@@ -12,12 +14,13 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <DevSupport ComponentPreviews={ComponentPreviews}
+        useInitialHook={useInitial}
+      >
+        <App />
+      </DevSupport>
     </Provider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
---
+++ src/routes.js
@@ -0,0 +1,8 @@
+import { useSelector } from 'react-redux';
+import { Navigate, useRoutes, useSearchParams } from 'react-router-dom';
+
+export default function Router(){
+    return useRoutes([
+        
+    ])
+}
