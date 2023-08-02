c99366ec60fa0a76f249af641d7fd13eca330aa2
Your Name <you@example.com>
2023-08-02 08:38:43 +0700

    xong navbar admin

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,13 @@
 import { useSelector } from 'react-redux';
 import { Navigate, useRoutes, useSearchParams } from 'react-router-dom';
+import AdminLayout from './layouts/AdminLayout';
 
-export default function Router(){
+export default function Router() {
     return useRoutes([
-        
+        {
+            path: '/admin',
+            element: <AdminLayout/>,
+
+        }
     ])
 }
