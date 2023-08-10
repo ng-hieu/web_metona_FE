3e3e4a241398b7c875b9a038bb0bd5cf0e352fe7
phong0710 <phonglh071099@gmail.com>
2023-08-10 17:22:00 +0700

    xong phần thêm product

--- src/components/forms/admin/AddProductForm.js
+++ src/components/forms/admin/AddProductForm.js
@@ -1,48 +1,54 @@
 import { Field, Form, Formik } from "formik";
 import {
-    Box,
-    Collapse,
-    FormControl,
-    IconButton,
-    Snackbar,
-    Stack,
-    TextField,
+    Box, Card, FormControl, Typography
 } from "@mui/material";
+import { TextField, Select } from 'formik-mui';
 import { LoadingButton } from "@mui/lab";
-import React, { useState } from "react";
-
+import React, { useEffect, useState } from "react";
+import MenuItem from "@mui/material/MenuItem";
 import * as Yup from "yup";
-import MuiAlert from "@mui/material/Alert";
 import { customAPIv1 } from "../../../features/APIv1/customAPI";
 
-export default function AddProductForm(props) {
+
+
+
+export default function AddProductForm() {
     const [openSuccess, setOpenSuccess] = useState(false);
     const [open, setOpen] = useState(false);
-    const handleClose = (event, reason) => {
-        if (reason === "clickaway") {
-            return;
-        }
-        setOpenSuccess(false);
-        setOpen(false);
-    };
-    const Alert = React.forwardRef(function Alert(props, ref) {
-        return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
-    });
     const SchemaError = Yup.object().shape({
-        name: Yup.string().min(2, "Tên Quá ngắn!").required("Required"),
-        price: Yup.string()
+        nameProduct: Yup.string().min(2, "Tên Quá ngắn!").required("Nhập tên sản phẩm"),
+        priceProduct: Yup.string()
             .min(2, "Vui lòng nhập đúng số!")
-            .required("Required"),
+            .required("Nhập giá sản phẩm"),
         quantityProduct: Yup.string()
             .min(2, "Vui lòng nhập đúng số!")
-            .required("Required"),
-        description: Yup.string().min(2, "Tên Quá ngắn!").required("Required"),
-        category: Yup.string().min(2, "Không được để trống!").required("Required"),
+            .required("Nhập số lượng sản phẩm"),
+        descriptionProduct: Yup.string().min(2, "Tên Quá ngắn!").required("Nhập mô tả sản phẩm"),
+        // category: Yup.string().min(2, "Không được để trống!").required("Thêm loại sản phẩm"),
     });
+
+    const [category, setCategory] = useState([]);
+    useEffect(() => {
+        customAPIv1()
+            .get("category")
+            .then((response) => {
+                console.log(response.data)
+                setCategory(response.data);
+            })
+            .catch((e) => {
+                console.log(e);
+            });
+    }, []);
 
     return (
         <Formik
-            initialValues={{}}
+            initialValues={{
+                nameProduct: "",
+                priceProduct: "",
+                quantityProduct: "",
+                descriptionProduct: "",
+                category: '',
+            }}
             validate={(values) => {
                 const errors = {};
                 return errors;
@@ -50,11 +56,10 @@ export default function AddProductForm(props) {
             validationSchema={SchemaError}
             onSubmit={(values, { setSubmitting, resetForm }) => {
                 console.log("trying to submit:", values);
-                // try {
                 customAPIv1()
                     .post("/products", values)
                     .then((response) => {
-                        console.log("Add product success");
+                        console.log("Add product success", response);
                         resetForm();
                         setSubmitting(false);
                         setOpenSuccess(true);
@@ -78,77 +83,84 @@ export default function AddProductForm(props) {
                 setFieldValue,
             }) => (
                 <Form>
-                    <TextField
-                        autoFocus
-                        margin="dense"
-                        id="name"
-                        label="Tên sản phẩm"
-                        type="email"
-                        fullWidth
-                        variant="standard"
-                    />
-
-                    <TextField
-                        autoFocus
-                        margin="dense"
-                        id="price"
-                        label="Giá sản phẩm"
-                        type="email"
-                        fullWidth
-                        variant="standard"
-                    />
-
-                    <TextField
-                        autoFocus
-                        margin="dense"
-                        id="quantityProduct"
-                        label="Số lượng"
-                        type="email"
-                        fullWidth
-                        variant="standard"
-                    />
-
-                    <TextField
-                        autoFocus
-                        margin="dense"
-                        id="description"
-                        label="Chi tiết sản phẩm "
-                        type="email"
-                        fullWidth
-                        variant="standard"
-                    />
-                    {/* <FormControl fullWidth>
+                    <Card sx={{ p: 5 }}>
                         <Field
-                            component={Select}
-                            id="category"
-                            name="category"
-                            labelId="category"
-                            label="Ngành hàng"
-                        >
-                            {category.map((item) => (
-                                <MenuItem value={item.id}>{item.name}</MenuItem>
-                            ))}
-                        </Field>
-                    </FormControl> */}
-                    <Box
-                        noValidate
-                        component="form"
-                        sx={{
-                            display: "flex",
-                            flexDirection: "column",
-                            m: "auto",
-                            width: "fit-content",
+                            component={TextField}
+                            type="nameProduct"
+                            label="Tên sản phẩm"
+                            name="nameProduct"
+                            variant="standard"
+                            fullWidth
+                        />
+
+                        <Field
+                            component={TextField}
+                            type="priceProduct"
+                            label="Giá sản phẩm"
+                            name="priceProduct"
+                            variant="standard"
+                            fullWidth
+                        />
+
+                        <Field
+                            component={TextField}
+                            type="quantityProduct"
+                            label="Số lượng sản phẩm"
+                            name="quantityProduct"
+                            variant="standard"
+                            fullWidth
+                        />
+
+                        <Field
+                            component={TextField}
+                            type="descriptionProduct"
+                            label="Mô tả"
+                            name="descriptionProduct"
+                            variant="standard"
+                            fullWidth
+                        />
+                        <Box sx={{ width: '100%' }}>
+                            <FormControl fullWidth sx={{ mt: 2 }}>
+                                <Field
+                                    component={Select}
+                                    id="category"
+                                    name="category"
+                                    label="Loại sản phẩm"
+                                    variant="outlined"
+                                >
+                                    {category && category.map(item => (
+
+                                        <MenuItem key={item.idCategory} value={item.idCategory}>
+                                            {item.nameCategory}
+                                        </MenuItem>
+                                    ))}
+                                </Field>
+                            </FormControl>
+                        </Box>
+                        <Box
+                            noValidate
+                            component="form"
+                            sx={{
+                                display: "flex",
+                                flexDirection: "column",
+                                m: "auto",
+                                width: "fit-content",
+                            }}
+                        ></Box>
+                        <Typography>
+
+                        </Typography>
+                        <LoadingButton fullWidth size="large" type="button" variant="contained" sx={{
+                            bgcolor: "#9a6348!important",
+                            '&:hover': {
+                                bgcolor: "#4c362b !important",
+                            },
                         }}
-                    ></Box>
-                    <LoadingButton
-                        fullWidth
-                        size="large"
-                        type="button"
-                        variant="contained"
-                        onClick={submitForm}
-                    >
-                        Thêm sẩn phẩm
-                    </LoadingButton>
+                            onClick={submitForm}>
+                            Thêm sản phẩm
+                        </LoadingButton>
+
+                    </Card>
                 </Form>
             )}
         </Formik>
--- src/features/APIv1/customAPI.js
+++ src/features/APIv1/customAPI.js
@@ -1,17 +1,8 @@
 import axios from "axios";
 
-export const customAPIv1 = () => {
-    console.log("process.env.REACT_APP_BE:", process.env.REACT_APP_BE);
-    return axios.create({
-        baseURL: process.env.REACT_APP_BE
-            ? process.env.REACT_APP_BE
-            : "http://127.0.0.1:8000/v1/",
-        headers: {
-            Authorization: `Bearer ${
-                localStorage.getItem("user")
-                    ? JSON.parse(localStorage.getItem("user")).token
-                    : ""
-            }`,
-        },
-    });
-};
+export const customAPIv1 = () => axios.create({
+    baseURL: 'http://127.0.0.1:8000/',
+    headers: {
+        Authorization: `Bearer ${(localStorage.getItem("user")) ? JSON.parse(localStorage.getItem("user")).token : ""}`
+    }
+})
